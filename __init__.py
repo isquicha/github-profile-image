ae545c7c0ab8e164da3fcd60fc71a3a23a0b8c85
Andre Vicente <isquicha@gmail.com>
2021-03-25 09:33:16 -0300

    feat: add get image logic

---
+++ __init__.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+from requests import get
+from requests.models import Response
+
+URL = "https://github.com"
+
+
+def _get_profile_page(uri: str) -> Response.content:
+    return get(uri).content
+
+
+def _get_image_link_from_page(page) -> str:
+    soup = BeautifulSoup(page, "html.parser")
+    image = soup.find("img", {"class": "avatar-user"})
+    image_link = image.get("src")
+    return image_link
+
+
+def get_user_image(user: str) -> str:
+    uri = f"{URL}/{user}"
+    page = _get_profile_page(uri=uri)
+    return _get_image_link_from_page(page)
